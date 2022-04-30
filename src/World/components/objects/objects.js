402c5ba046f4425df593a044f8e46901c2f627cf
Roberto <alesi.roberto@gmail.com>
2022-04-30 13:28:19 +0200

    sd

--- src/World/components/objects/objects.js
+++ src/World/components/objects/objects.js
@@ -8,7 +8,7 @@ async function loadObjects() {
   let object;
 
   if (objectID == 0) {
-    const data = await loader.loadAsync ('tie.glb');
+    const data = await loader.loadAsync ('https://vivien000.github.io/trompeloeil/models/tie.glb');
     object = setupModel(data);
     object.scale.set(0.00002, 0.00002, 0.00002);
     object.position.set(0, 0, -0.05);
@@ -18,7 +18,7 @@ async function loadObjects() {
     object.scale.set(0.15, 0.15, 0.15);
     object.position.set(0, 0, -0.02);
   } else {
-    const data = await loader.loadAsync('https://vivien000.github.io/trompeloeil/models/vangogh.glb');
+    const data = await loader.loadAsync('vangogh.glb');
     object = setupModel(data);
     object.scale.set(0.2, 0.2, 0.2);
     object.position.set(0, -0.3, -0.5);
